9bf6039caecb6cf507a05444b1e874d67139bf1c
SaintThorax <tfpvoort@gmail.com>
2017-01-14 17:05:29 +0100

    Nieuwere versie, straks update

--- src/bke/BKE.java
+++ src/bke/BKE.java
@@ -1,135 +1,148 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * probeer UI class te veranderen in een new jpanel van class BKE
+ * 
+ * 
  */
 package bke;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class BKE extends JFrame {
+
+    public static Font spelFont = new Font("Berlin Sans FB", Font.PLAIN, 100);
+    public static Font textFont = new Font("Arial", Font.PLAIN, 20);
     
-    JPanel cards;
+    public static JLabel scoreLabX, scoreLabO;
+    private JButton reset, menuKnop, tegenSpeler;
+    public static JButton[][] vakken;
+    public static JTextField gewonnen;
     static JFrame frame;
-    private JButton menu;
       
     public static void main(String[] args) {        
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        createGUI();
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                System.out.println("Test");
+                new BKE().createSpelGUI();
+            }
+        });
     }
-    
-    public static void createGUI(){
-
-                
-        Container pane = frame.getContentPane();
-        pane.setLayout(new BorderLayout(60,60));
+    public void createMenuGUI(){
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        pane.add(button, BorderLayout.PAGE_START);
-        button.setPreferredSize(new Dimension(0, 0));
-
-        button = new JButton("Button 3 (LINE_START)");
-        pane.add(button, BorderLayout.LINE_START);
-        button.setPreferredSize(new Dimension(0, 0));   
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        pane.add(button, BorderLayout.PAGE_END);
-        button.setPreferredSize(new Dimension(0, 0));
-        button = new JButton("5 (LINE_END)");
-        pane.add(button, BorderLayout.LINE_END);
-        button.setPreferredSize(new Dimension(0, 0));
-        
-        //contentPane.setSize(400,400);
-        pane.add(new Spel());
-        
-        BKE bke = new BKE();     
-        
-        frame.setSize(600,600);
-        frame.setTitle("Quick and Dirty");
-        frame.setVisible(true);      
-    }
-    
-    public void naarMenu(){
-        System.out.println("Freeze all motor functions");
-        frame.setContentPane(new Menu());
-        frame.revalidate();
-    }
-    public void naarSpelerS(){
-        System.out.println("De-freeze all motor functions");
-        this.dispose();
-        createGUI();
-    }    
-}
-
-
-class Menu extends JPanel{
-    private JButton tegenSpeler;
-    
-    
-    public Menu(){
+        JPanel Menu = new JPanel();
+      
         tegenSpeler = new JButton("Speler - Speler");
-        tegenSpeler.addActionListener(new CODHandeler());
+        tegenSpeler.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+            createSpelGUI();
+        }
+    });
+
+        Menu.add(tegenSpeler);    
         
-        add(tegenSpeler);
+        setContentPane(Menu);
+        setSize(600,600);
+        setTitle("Quick and Dirty");
+        setVisible(true); 
     }
-    
-    class CODHandeler implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            BKE speler = new BKE();
-            speler.naarSpelerS();
+        
+    public void createSpelGUI(){
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        JPanel UI = new JPanel();
+        UI.setLayout(new BorderLayout());
+//        gewonnen = new JTextField(10);
+//        gewonnen.setFont(textFont);
+//        gewonnen.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        scoreLabX = new JLabel();
+        reset = new JButton("Reset");
+        reset.addActionListener(new ResetHandeler());
+        
+        menuKnop = new JButton("Menu");
+        menuKnop.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                createMenuGUI();
+            }
+        });
+        
+        UI.add(menuKnop, BorderLayout.LINE_START);
+        UI.add(scoreLabX, BorderLayout.CENTER);
+        UI.add(reset, BorderLayout.LINE_END);
+        UI.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        
+        JPanel spelBord = new JPanel();
+        //spelBord.setBorder(BorderFactory.createEmptyBorder(10,20,0,20));
+
+        spelBord.setLayout(new BorderLayout(60,40));
+        
+//        JButton button = new JButton("Button 3 (LINE_START)");
+//        spelBord.add(button, BorderLayout.LINE_START);
+//        button.setPreferredSize(new Dimension(0, 0));   
+//        button = new JButton("5 (LINE_END)");
+//        spelBord.add(button, BorderLayout.LINE_END);
+//        button.setPreferredSize(new Dimension(0, 0));
+        
+        spelBord.add(UI, BorderLayout.PAGE_START);
+        UI.setPreferredSize(new Dimension(0, 100));
+
+        Spel TEST = new Spel();
+        
+        TEST.setBorder(BorderFactory.createEmptyBorder(10,50,0,50));
+        //TEST.setLayout(new BorderLayout());
+        
+        spelBord.add(TEST);
+        //spelBord.add(new Spel());
+        
+        setContentPane(spelBord);
+        setSize(600,600);
+        setTitle("Quick and Dirty");
+        setVisible(true);      
+    }
+
+    class ResetHandeler implements ActionListener{        
+    public void actionPerformed(ActionEvent e){
+        for (int i =0; i<3; i++){
+            for (int j=0; j<3;j++){
+                vakken[i][j].setEnabled(true);
+                vakken[i][j].setText("");
+                Spel.aantal_geklikt = 0;
+                Spel.aantal_zetten = 0;
+                Spel.over = false;
+                }
+            }       
         }
     }
 }
 
 class Spel extends JPanel{
-    
-    private JButton[][] vakken;
-    private Font spelFont = new Font("Berlin Sans FB", Font.PLAIN, 100);
-    private Font textFont = new Font("Arial", Font.PLAIN, 20);
-    
-    private boolean over = false;
-    private JButton reset, menu;
-    private int aantal_geklikt = 0, aantal_zetten = 0;
-    private JTextField gewonnen;
     final String X = "X", O = "O";
-    private JFrame BKE;
-    
+    public static boolean over = false;
+    public static int scoreIntX, scoreIntO;
+    public static int aantal_geklikt = 0, aantal_zetten = 0;
     public Spel(){
-               
+
         setLayout(new GridLayout(4,3,5,5));
-        
-        gewonnen = new JTextField(10);
-        gewonnen.setFont(textFont);
-        gewonnen.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        reset = new JButton("Reset");
-        reset.addActionListener(new ResetHandeler());
-        
-        menu = new JButton("Menu");
-        menu.addActionListener(new MenuHandeler());
-        
-        add(menu);
-        add(gewonnen);
-        add(reset);
 
-
-        vakken = new JButton[3][3];
+        BKE.vakken = new JButton[3][3];
         for (int i=0; i<3; i++){
             for (int j = 0; j<3;j++){
-                vakken[i][j] = new JButton("");
-                vakken[i][j].addActionListener(new KnopHandeler());
-                add(vakken[i][j]);
+                BKE.vakken[i][j] = new JButton("");
+                BKE.vakken[i][j].addActionListener(new KnopHandeler());
+                add(BKE.vakken[i][j]);
             }
         }
+
     }
+    
+
     class KnopHandeler implements ActionListener{
         public void actionPerformed(ActionEvent e){
 
             
             JButton geklikt = (JButton)e.getSource();
-            geklikt.setFont(spelFont);
+            geklikt.setFont(BKE.spelFont);
             
             aantal_geklikt++;
             aantal_zetten++;
@@ -142,67 +155,49 @@ class Spel extends JPanel{
             for(int i = 0;i<3;i++){
                 for (int j=0;j<3;j++){
                     //vakken[verticaal][horizontaal]
-                    if(vakken[j][0].getText().equals(X) && vakken[j][1].getText().equals(X) && vakken[j][2].getText().equals(X)){
-                        gewonnen.setText("X wint!");
+                    if(BKE.vakken[j][0].getText().equals(X) && BKE.vakken[j][1].getText().equals(X) && BKE.vakken[j][2].getText().equals(X)){
+                        //BKE.gewon1 1 nen.setText("X wint!");
+                        scoreIntX =+ 1;
+                        String scoreStrX = String.valueOf(scoreIntX);
+                        BKE.scoreLabX.setText(scoreStrX);
                         over = true;
                     } 
-                    if(vakken[0][i].getText().equals(X) && vakken[1][i].getText().equals(X) && vakken[2][i].getText().equals(X)){
-                        gewonnen.setText("X wint!");
+                    if(BKE.vakken[0][i].getText().equals(X) && BKE.vakken[1][i].getText().equals(X) && BKE.vakken[2][i].getText().equals(X)){
+                        //BKE.gewonnen.setText("X wint!");
                         over = true;
                     }
-                    if(vakken[2][0].getText().equals(X) && vakken[1][1].getText().equals(X) && vakken[0][2].getText().equals(X) ||
-                       vakken[0][0].getText().equals(X) && vakken[1][1].getText().equals(X) && vakken[2][2].getText().equals(X)){
-                        gewonnen.setText("X wint!");
+                    if(BKE.vakken[2][0].getText().equals(X) && BKE.vakken[1][1].getText().equals(X) && BKE.vakken[0][2].getText().equals(X) ||
+                       BKE.vakken[0][0].getText().equals(X) && BKE.vakken[1][1].getText().equals(X) && BKE.vakken[2][2].getText().equals(X)){
+                        //BKE.gewonnen.setText("X wint!");
                         over = true;
                     }
                     
                     //Horizontale winconditie voor speler O
-                    if (vakken[j][0].getText().equals(O) && vakken[j][1].getText().equals(O) && vakken[j][2].getText().equals(O)){
-                        gewonnen.setText("O wint!");
+                    if (BKE.vakken[j][0].getText().equals(O) && BKE.vakken[j][1].getText().equals(O) && BKE.vakken[j][2].getText().equals(O)){
+                        //BKE.gewonnen.setText("O wint!");
                         over = true;
                     } // Verticale winconditie voor speler O
-                    if(vakken[0][i].getText().equals(O) && vakken[1][i].getText().equals(O) && vakken[2][i].getText().equals(O)){
-                        gewonnen.setText("O wint!");
+                    if(BKE.vakken[0][i].getText().equals(O) && BKE.vakken[1][i].getText().equals(O) && BKE.vakken[2][i].getText().equals(O)){
+                        //BKE.gewonnen.setText("O wint!");
                         over = true;
                     } //Diagonale winconditie voor speler O
-                    if(vakken[2][0].getText().equals(O) && vakken[1][1].getText().equals(O) && vakken[0][2].getText().equals(O) ||
-                       vakken[0][0].getText().equals(O) && vakken[1][1].getText().equals(O) && vakken[2][2].getText().equals(O)){
-                        gewonnen.setText("O wint!");
+                    if(BKE.vakken[2][0].getText().equals(O) && BKE.vakken[1][1].getText().equals(O) && BKE.vakken[0][2].getText().equals(O) ||
+                       BKE.vakken[0][0].getText().equals(O) && BKE.vakken[1][1].getText().equals(O) && BKE.vakken[2][2].getText().equals(O)){
+                        //BKE.gewonnen.setText("O wint!");
                         over = true;
                     }
                     
                     //Gelijk spel conditie
-                    if(aantal_zetten >= 9){
-                        gewonnen.setText("Gelijkspel");
+                    if(aantal_zetten >= 9 && over == false){
+                        //BKE.gewonnen.setText("Gelijkspel");
                         over = true;
-                    }//Schakelt alle 
+                    }//Schakelt alle vakken uit
                     if(over == true){
-                        vakken[i][j].setEnabled(false);
+                        BKE.vakken[i][j].setEnabled(false);
+                        //System.out.println(BKE.vakken[i][j]);
                     }
                 }
             }
         }
     }
-    
-    class ResetHandeler implements ActionListener{        
-        public void actionPerformed(ActionEvent e){
-            for (int i =0; i<3; i++){
-                for (int j=0; j<3;j++){
-                    vakken[i][j].setEnabled(true);
-                    vakken[i][j].setText("");
-                    gewonnen.setText("");
-                    aantal_zetten = 0;
-                    aantal_geklikt = 0;
-                    aantal_zetten = 0;
-                    over = false;
-                }
-            }       
-        }
-    }
-    class MenuHandeler implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            BKE menu = new BKE();
-            menu.naarMenu();
-        }
-    }
-}
+} 
