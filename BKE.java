24ec1b4a723eabcc506777c29f6c31ba799fc6ea
SaintThorax <SaintThorax@users.noreply.github.com>
2017-01-17 16:41:45 +0100

    Add files via upload

---
+++ BKE.java
@@ -0,0 +1,195 @@
+/*
+ * 
+ * 
+ * 
+ */
+package bke;
+import java.awt.*;
+import java.awt.Font;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class BKE extends JFrame {
+
+    public static Font spelFont = new Font("Berlin Sans FB", Font.PLAIN, 100);
+    public static Font scoreLetterFont = new Font("Helvetica", Font.BOLD, 130);
+    public static Font scoreNummerFont = new Font("Century Gothic", Font.PLAIN, 70);
+    public static Font titelFont = new Font("Arial", Font.PLAIN, 60);
+    public static Font algemeenFont = new Font("Century Gothic", Font.BOLD, 20);
+    
+    private JPanel scoreX, scoreO;
+    public static JLabel scoreLetterX, scoreNummerX, scoreLetterO, scoreNummerO, Titel;
+    private JButton reset, menuKnop, tegenSpeler;
+    public static JButton[][] vakken;
+    static JFrame frame;
+      
+    public static void main(String[] args) {        
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                new BKE().createMenuGUI();
+            }
+        });
+    }
+    public void createMenuGUI(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+              
+        Titel = new JLabel("Boter-kaas-en-eieren", SwingConstants.CENTER);
+        Titel.setFont(titelFont);
+        Titel.setPreferredSize(new Dimension(600,100));
+        
+        JPanel Menu = new JPanel();
+        JPanel keuzes = new JPanel();
+        keuzes.setBorder(BorderFactory.createEmptyBorder(100,0,0,0));
+      
+        tegenSpeler = new JButton("Speler - Speler");
+        tegenSpeler.setFont(algemeenFont);
+        tegenSpeler.setFocusPainted(false);
+        tegenSpeler.setBackground(Color.decode("#ECF0F1"));
+        tegenSpeler.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
+        tegenSpeler.setPreferredSize(new Dimension(500,100));
+        tegenSpeler.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                createSpelGUI();
+            }
+        });
+        
+        keuzes.add(tegenSpeler);
+        Menu.add(Titel);
+        Menu.add(keuzes);    
+        
+        setContentPane(Menu);
+        setSize(800,800);
+        setTitle("Quick and Dirty");
+        setVisible(true); 
+    }
+        
+    public void createSpelGUI(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        scoreCreateX();
+        scoreCreateO();
+        
+        JPanel score = new JPanel();
+
+        score.setPreferredSize(new Dimension(10,100));
+        score.setLayout(new GridLayout(1,2,5,5));
+        score.setBorder(BorderFactory.createEmptyBorder(10,10,0,10));
+       
+        score.add(scoreX);
+        score.add(scoreO);
+        
+        JPanel UI = new JPanel();
+        UI.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        UI.setLayout(new BorderLayout());
+        
+        reset = new JButton("Nieuw spel");
+        reset.setBackground(Color.decode("#ECF0F1"));       
+        reset.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
+        reset.addActionListener(new ResetHandeler());
+        
+        menuKnop = new JButton("Menu");
+        menuKnop.setFocusPainted(false);
+        menuKnop.setBackground(Color.decode("#ECF0F1"));
+        menuKnop.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
+        menuKnop.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                createMenuGUI();
+            }
+        });
+        menuKnop.addActionListener(new ResetHandeler());
+             
+        UI.add(menuKnop);
+        UI.add(reset);
+        
+        for (Component c : UI.getComponents()){
+            c.setFont(algemeenFont);
+        }
+        UI.setPreferredSize(new Dimension(15, 100));
+        UI.setLayout(new GridLayout(1,2,10,0));
+
+        JPanel spelBord = new JPanel();
+        spelBord.setLayout(new BorderLayout(60,40));
+        
+        JPanel test = new JPanel();
+        test.setBorder(BorderFactory.createEmptyBorder(0,50,0,50));
+        test.setLayout(new GridLayout(10,10,10,10));
+        Spel spel = new Spel();
+        //spel.setBackground(Color.decode("#d2d2d2"));
+
+        test.setLayout(new GridLayout(1,1,50,5));
+        test.setPreferredSize(new Dimension(100, 100));
+       
+        //spel.setBorder(BorderFactory.createLineBorder(Color.decode("#d2d2d2"), 4));
+        test.add(spel);
+        
+        spelBord.add(score, BorderLayout.PAGE_START);
+        spelBord.add(UI, BorderLayout.PAGE_END);
+        spelBord.add(test);
+        
+        for (Component c : spelBord.getComponents()){
+            c.setBackground(Color.decode("#ECF0F1"));
+        }
+        spelBord.setBackground(Color.decode("#ECF0F1"));
+        
+        setContentPane(spelBord);
+        setSize(800,800);
+        setTitle("Quick and Dirty");
+        setVisible(true);      
+    }
+    
+    public void scoreCreateX(){
+        scoreX = new JPanel();
+        
+        scoreLetterX = new JLabel(Spel.X, SwingConstants.LEFT);
+        scoreNummerX = new JLabel("-  ", SwingConstants.RIGHT);
+        
+        scoreLetterX.setFont(scoreLetterFont);
+        scoreNummerX.setFont(scoreNummerFont);
+        
+        scoreLetterX.setForeground(Color.decode("#2C3E50"));
+        scoreNummerX.setForeground(Color.decode("#2d2d2d"));
+        
+        scoreX.setLayout(new GridLayout(1,2));
+        scoreX.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
+        
+        scoreX.add(scoreLetterX);
+        scoreX.add(scoreNummerX);
+    }
+    
+    public void scoreCreateO(){
+        scoreO = new JPanel();
+        
+        scoreLetterO = new JLabel(Spel.O, SwingConstants.LEFT);
+        scoreNummerO = new JLabel("- ", SwingConstants.RIGHT);
+        
+        scoreLetterO.setFont(scoreLetterFont);
+        scoreNummerO.setFont(scoreNummerFont);
+        
+        scoreLetterO.setForeground(Color.decode("#E74C3C"));
+        scoreNummerO.setForeground(Color.decode("#2d2d2d"));        
+        
+        scoreO.setLayout(new GridLayout(1,2));
+        scoreO.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, true));
+        
+        scoreO.add(scoreLetterO);
+        scoreO.add(scoreNummerO);
+    }
+
+    class ResetHandeler implements ActionListener{        
+    public void actionPerformed(ActionEvent e){
+        for (int i =0; i<3; i++){
+            for (int j=0; j<3;j++){
+                vakken[i][j].setEnabled(true);
+                vakken[i][j].setBackground(Color.decode("#ECF0F1"));
+                //UIManager.put("Button.disabledText", Color.decode("#263248"));
+                vakken[i][j].setText("");
+                Spel.aantal_geklikt = 0;
+                Spel.over = false;
+                Spel.xWin = false;
+                Spel.oWin = false;
+                }
+            }       
+        }
+    }
+}
+
---
+++ Spel.java
@@ -0,0 +1,152 @@
+package bke;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ *
+ * @author Thom
+ */
+public class Spel extends JPanel{
+    final static String X = "X", O = "O";
+    public static boolean over = false, xWin = false, oWin = false;
+    public static int scoreIntX, scoreIntO, test = 3;
+    private int boven, links, onder, rechts;
+    public static int aantal_geklikt = 0;
+    public Spel(){
+
+        setLayout(new GridLayout(3,3,5,5));
+        BKE.vakken = new JButton[3][3];
+        int tel = 0;
+        for (int i=0; i<3; i++){
+            for (int j = 0; j<3;j++){
+                BKE.vakken[i][j] = new JButton();
+                BKE.vakken[i][j].setFont(BKE.spelFont);
+                BKE.vakken[i][j].setBackground(Color.decode("#ECF0F1"));
+                if (tel > 2)        boven = 3;
+                if (tel % 3 != 0)   links = 3;
+                if (tel < 6)        onder = 3;
+                if (tel % 3 != 2)   rechts = 3;
+                BKE.vakken[i][j].addActionListener(new KnopHandeler());
+                BKE.vakken[i][j].setBorder(BorderFactory.createMatteBorder(boven,links,onder,rechts,Color.decode("#2a2a2a")));
+                
+                boven = 0;
+                links = 0;
+                onder = 0;
+                rechts=0;
+                
+                add(BKE.vakken[i][j]);
+                tel++;
+            }
+        }
+    }
+
+
+    class KnopHandeler implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            JButton geklikt = (JButton)e.getSource();
+            aantal_geklikt++;
+
+            if (aantal_geklikt % 2 != 0) {
+                UIManager.put("Button.disabledText", Color.decode("#2C3E50"));
+                geklikt.setText("X");
+            }
+            if (aantal_geklikt % 2 == 0) {
+                UIManager.put("Button.disabledText", Color.decode("#E74C3C"));
+                geklikt.setText("O");
+            }
+
+            geklikt.setEnabled(false);
+            for(int i = 0;i<3;i++){
+                for (int j=0;j<3;j++){                    
+                    //vakken[verticaal][horizontaal]
+                    if(BKE.vakken[j][0].getText().equals(X) && BKE.vakken[j][1].getText().equals(X) && BKE.vakken[j][2].getText().equals(X)){
+                        BKE.vakken[j][i].setBackground(Color.decode("#2C3E50"));
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        over = true;
+                        xWin = true;
+                    } 
+                    if(BKE.vakken[0][i].getText().equals(X) && BKE.vakken[1][i].getText().equals(X) && BKE.vakken[2][i].getText().equals(X)){
+                        BKE.vakken[j][i].setBackground(Color.decode("#2C3E50"));
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        xWin = true;
+                        over = true;
+                    }
+                    if(BKE.vakken[2][0].getText().equals(X) && BKE.vakken[1][1].getText().equals(X) && BKE.vakken[0][2].getText().equals(X)){
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        xWin = true;
+                        over = true;
+                        for (int n = 0; n < 3 ; n++){
+                            BKE.vakken[2-n][n].setBackground(Color.decode("#2C3E50"));
+                            System.out.println(n);
+                        } break;
+                    }
+                    if (BKE.vakken[0][0].getText().equals(X) && BKE.vakken[1][1].getText().equals(X) && BKE.vakken[2][2].getText().equals(X)){
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        xWin = true;
+                        over = true;
+                        for (int n = 0; n < 3 ; n++){
+                            BKE.vakken[n][n].setBackground(Color.decode("#2C3E50"));
+                            System.out.println(n);    
+                        } break;
+                    }
+
+                    //Horizontale winconditie voor speler O
+                    if (BKE.vakken[j][0].getText().equals(O) && BKE.vakken[j][1].getText().equals(O) && BKE.vakken[j][2].getText().equals(O)){
+                        BKE.vakken[j][i].setBackground(Color.decode("#E74C3C"));               
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        oWin = true;
+                        over = true;
+                    } // Verticale winconditie voor speler O
+                    if(BKE.vakken[0][i].getText().equals(O) && BKE.vakken[1][i].getText().equals(O) && BKE.vakken[2][i].getText().equals(O)){
+                        BKE.vakken[j][i].setBackground(Color.decode("#E74C3C"));
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        oWin = true;
+                        over = true;
+                    } //Diagonale winconditie voor speler O
+                    if(BKE.vakken[2][0].getText().equals(O) && BKE.vakken[1][1].getText().equals(O) && BKE.vakken[0][2].getText().equals(O)){
+                       UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        oWin = true;
+                        over = true;
+                        for (int n = 0; n < 3 ; n++){
+                            BKE.vakken[2-n][n].setBackground(Color.decode("#E74C3C"));
+                            System.out.println(n);    
+                        } break; 
+                    }
+                    if(BKE.vakken[0][0].getText().equals(O) && BKE.vakken[1][1].getText().equals(O) && BKE.vakken[2][2].getText().equals(O)){
+                        UIManager.put("Button.disabledText", Color.decode("#FFFFFF"));
+                        oWin = true;
+                        over = true;
+                        for (int n = 0; n < 3 ; n++){
+                            BKE.vakken[n][n].setBackground(Color.decode("#E74C3C"));
+                            System.out.println(n);    
+                        } break;
+                    }
+
+                    //Gelijk spel conditie
+                    if(aantal_geklikt >= 9 && over == false){
+                        over = true;
+                    }//Schakelt alle vakken uit
+                }
+            } 
+            if(over == true){
+                System.out.println("Spel is over");             
+                for(int i = 0;i<3;i++){
+                    for (int j=0;j<3;j++){
+                        BKE.vakken[i][j].setEnabled(false);
+                    }
+                }
+            }
+
+            if(xWin == true){
+                scoreIntX = scoreIntX + 1;
+                String scoreStrX = String.valueOf(scoreIntX + " ");
+                BKE.scoreNummerX.setText(scoreStrX);
+            }
+            if(oWin == true){
+                scoreIntO = scoreIntO + 1;
+                String scoreStrO = String.valueOf(scoreIntO + " ");
+                BKE.scoreNummerO.setText(scoreStrO);
+            }
+        }
+    }
+} 
